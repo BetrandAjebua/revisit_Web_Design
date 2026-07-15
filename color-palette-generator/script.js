7a868288454934c06765f16e847d83f2871b4eff
Betrand Ajebua <betrandajebua@gmail.com>
2026-07-15 01:44:13 +0100

    JavaScript Fully Functional with generate color and copy of color

--- color-palette-generator/script.js
+++ color-palette-generator/script.js
@@ -1,38 +1,59 @@
-document.querySelector('.palette-btn').addEventListener("click", generatePalet);
+document.querySelector(".palette-btn").addEventListener("click", generatePalet);
 
-document.querySelectorAll('.color-box').forEach((element)=>{
-  element.addEventListener("click", copyColor)
-})
-function copyColor(){
-  console.log(event.target.style.backgroundColor)
+document.querySelectorAll(".color-box").forEach((element) => {
+  element.addEventListener("click", (event) => {
+    copyColor(event);
+  });
+});
+function copyColor(event) {
+  if (event.target.classList.contains("fa-copy")) {
+    navigator.clipboard
+      .writeText(event.currentTarget.textContent)
+      .then(sucess(event.target))
+      .catch((err) => {
+        alert(err);
+      });
+  }else if (event.target.classList.contains("color")) {
+     navigator.clipboard
+      .writeText(event.currentTarget.textContent)
+      .then(sucess(event.currentTarget.querySelector(".fa")))
+      .catch((err) => {
+        alert(err);
+      });
 }
-// function copyColor(){
-//   alert("Copy Init")
-// }
-generatePalet()
-function generatePalet(){
-  const colors= []
-  for(let i=0 ; i<6 ; i++){
-    colors.push(generateColor())
+  
+}
+  // function copyColor(){
+  //   alert("Copy Init")
+  // }
+  function sucess(icon) {
+    icon.classList.toggle("fa-copy");
+    icon.classList.toggle("fa-check");
+    setTimeout(() => {
+      icon.classList.toggle("fa-copy");
+      icon.classList.toggle("fa-check");
+    }, 1500);
   }
-document.querySelectorAll(".color").forEach((element, index)=>{
-
-  element.style.backgroundColor = "#"+colors[index]
-
-})
-  document.querySelectorAll(".color-code").forEach((element, index)=>{
-
-  element.textContent = "#"+colors[index]
-
-})
-}
-
-function generateColor(){
-  const colorDigit = '1234567890abcdef';
-  let color="";
-  for(i = 1; i<7 ; i++){
-    color+=colorDigit[Math.floor(Math.random()*15)];
-    
+  generatePalet();
+  function generatePalet() {
+    const colors = [];
+    for (let i = 0; i < 6; i++) {
+      colors.push(generateColor());
+    }
+    document.querySelectorAll(".color").forEach((element, index) => {
+      element.style.backgroundColor = "#" + colors[index];
+    });
+    document.querySelectorAll(".color-code").forEach((element, index) => {
+      element.textContent = "#" + colors[index];
+    });
   }
-return (color)
-}
+
+  function generateColor() {
+    const colorDigit = "1234567890abcdef";
+    let color = "";
+    for (i = 1; i < 7; i++) {
+      color += colorDigit[Math.floor(Math.random() * 15)];
+    }
+    return color;
+  }
+
